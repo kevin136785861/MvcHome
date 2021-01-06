3e646a125b2f8a40af7f93567c8a2770fd7c9c0b
kevinzhou1998 <136785861@qq.com>
2021-01-06 16:58:45 +0800

    完成了MyBatis代码反向生成器

---
+++ src/main/java/demo/dao/ProductMapper.java
@@ -0,0 +1,22 @@
+package demo.dao;
+
+import demo.domain.Product;
+import demo.domain.ProductExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ProductMapper {
+    long countByExample(ProductExample example);
+
+    int deleteByExample(ProductExample example);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    List<Product> selectByExample(ProductExample example);
+
+    int updateByExampleSelective(@Param("record") Product record, @Param("example") ProductExample example);
+
+    int updateByExample(@Param("record") Product record, @Param("example") ProductExample example);
+}
